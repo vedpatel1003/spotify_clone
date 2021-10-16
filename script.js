10e397666cfa28de9723485e324a355e03222a84
vedpatel1003 <vedpatel987612@gmail.com>
2021-10-17 01:21:22 +0530

    Added new files

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterplay = document.getElementyById('masterplay');
+let myprogressbar = document.getElementById('myprogressbar');
+
+
+let songs = [
+    { songname="Into Your Arms", filepath: "c:\Users\Admin\Desktop\Code Playground", coverpath: "covers/1.jpg" },
+    { songname="Into Your Arms", filepath: "C:\Users\Admin\Desktop\Code Playground", coverpath: "covers/1.jpg" },
+    { songname="Into Your Arms", filepath: "C:\Users\Admin\Desktop\Code Playground", coverpath: "covers/1.jpg" },
+    { songname="Into Your Arms", filepath: "C:\Users\Admin\Desktop\Code Playground", coverpath: "covers/1.jpg" },
+    { songname="Into Your Arms", filepath: "C:\Users\Admin\Desktop\Code Playground", coverpath: "covers/1.jpg" },
+    { songname="Into Your Arms", filepath: "C:\Users\Admin\Desktop\Code Playground", coverpath: "covers/1.jpg" },
+    { songname="Into Your Arms", filepath: "C:\Users\Admin\Desktop\Code Playground", coverpath: "covers/1.jpg" },
+    { songname="Into Your Arms", filepath: "C:\Users\Admin\Desktop\Code Playground", coverpath: "covers/1.jpg" },
+]
+// audioElement,play();
+
+//handle play/pause click
+masterplay.addEventListener('click',()=>{
+    if (audioElement.paused || audioElement.currentTime <= 0){
+        audioElement.play();
+    }
+})
+
+//Listen to Events
+myprogressbar.addEventListener('timeupdate', () => {
+    console.log('timeupdate');
+    //update seekbar
+})
+
